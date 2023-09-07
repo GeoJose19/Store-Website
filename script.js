be0425a69cf14c74ddd6a3d2b25b035d30f5df59
Geo Jose P <97250703+GeoJose19@users.noreply.github.com>
2023-09-07 17:27:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/*
 const menuBtn=document.querySelector('.menuBtn')
 const navlinks=document.querySelector('.nav-links')
 
 menuBtn.addEventListener('click',()=>{
     navlinks.classList.toggle('active')
 })
+*/
